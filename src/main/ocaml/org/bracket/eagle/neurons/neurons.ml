1dea286a618cc27d8d5432d169102b784a5c90e3
Snaipe <franklinmathieu@gmail.com>
2013-10-03 19:36:54 +0200

    Made the damn thing compile & first front-propagation draft.

--- src/main/ocaml/org/bracket/eagle/neurons/neurons.ml
+++ src/main/ocaml/org/bracket/eagle/neurons/neurons.ml
@@ -1,54 +1,47 @@
-class neuron =
-    object (this)
-        val mutable version = 0
-        val mutable value = 0
-        val mutable inputs  = ([] : link list)
-        val mutable outputs = ([] : link list)
-        method sigma x = 
-            1. /. (1. +. exp(-x))
-        method back_propagation =
-            ()
-    end
-and link =
-    object (this)
-        val mutable weight = 0
-        val mutable input  = (None : neuron option)
-        val mutable output = (None : neuron option)
-        method get_input = 
-            input
-        method get_output = 
-            output
-        method set_input value =
-            input <- value
-        method set_output value =
-            output <- value
-        method get_weight = 
-            weight
-        method set_weight value =
-            weight <- value
-    end;;
+type neuron = {
+    mutable version : int;
+    mutable value : int;
+    mutable inputs : link list;
+    mutable outputs : link list
+} and link = {
+    mutable weight : int;
+    mutable input : neuron;
+    mutable output : neuron
+}
+
+let contents = function
+    | Some c -> c
+    | None   -> raise (Invalid_argument "None has no contents")
 
 class network =
     object (this)
-        val mutable inputs  = (Array.make (28 * 28) None : neuron array)
-        val mutable outputs = (Array.make (2*26 + 10) None : neuron array)
-        val mutable hidden  = (Array.make 96 None : neuron array) (* 96 neurons *)
-        method add_input neuron =
-            inputs <- inputs::neuron
-        method add_output neuron =
-            outputs <- outputs::neuron
+        val mutable inputs  = (Array.make (28 * 28) None : neuron option array)
+        val mutable outputs = (Array.make (2*26 + 10) None : neuron option array)
+        val mutable hidden  = (Array.make 96 None : neuron option array) (* 96 neurons *)
+        
+        method sigma (x : float) =
+            1. /. (1. +. exp(-. x))
+
+        method set_input i neuron =
+            inputs.(i) <- neuron
+        
+        method set_output i neuron =
+            outputs.(i) <- neuron
+        
         method set_values (l : int array) = let sum = ref 0 in (
-            for i = 0 to (Array.length inputs) do (
-                Array.set inputs i (Array.get i l)
-            ) done;
-            for i = 0 to (Array.length hidden) do (
-                let rec set_hidden v = 
-                    match v#get_inputs with
-                        | e::l -> 
-                            (sum := sum + e#get_weight * e#get_input#get_value)
-                        |   [] -> ()
-                in set_hidden (Array.get hidden i)
-            ) done;
+            Array.iteri (fun e i -> if inputs.(i) <> None then (contents inputs.(i)).value <- e) l;
+            let recalculate (neurons : neuron option array) = 
+                Array.iter (fun e1 -> 
+                    if (e1 <> None) then (
+                        List.iter (fun e2 -> 
+                            sum := !sum + e2.weight * e2.input.value
+                        ) (contents e1).inputs;
+                        (contents e1).value <- (int_of_float (this#sigma (float_of_int !sum)));
+                        sum := 0;
+                        (contents e1).version <- (contents e1).version + 1
+                   )
+                ) neurons in
+            (recalculate hidden; recalculate outputs)
         )
-    end;;
+    end
 
