53d8e14f085e107b9d9d7cb7452cc2ff9fb555ef
Snaipe <franklinmathieu@gmail.com>
2013-10-18 14:27:04 +0200

    reorganized structure

--- src/main/ocaml/org/bracket/eagle/neurons/neuron.ml
+++ src/main/ocaml/org/bracket/eagle/neurons/neuron.ml
@@ -11,26 +11,39 @@ type neuron = {
 
 let new_neuron () = {version = 0; value = 0.; parents = []; children = []}
 
+let fabs x = if x < 0. then x else -. x
+
 class network epsilon (inputs : neuron array) (outputs : neuron array) (hidden : neuron array) =
     object (this)
         method sigma (x : float) =
-            1. /. (1. +. exp(-. x))
+            x /. (1. +. fabs(x))
 
         method set_values (l : float array) = 
             let sum = ref 0. in 
-                Array.iteri (fun index v -> inputs.(index).value <- v) l;
+                Printf.printf "Input neurons :\n";
+                Array.iteri (fun index v -> Printf.printf "n%d -> %f\n" index v; inputs.(index).value <- v) l;
+                print_newline ();
                 let recalculate neurons = 
-                    Array.iter (fun n -> 
-                        List.iter (fun p -> 
+                    Array.iteri (fun i n ->
+                        List.iter (fun p ->
                             sum := !sum +. p.weight *. p.entry.value
                         ) n.parents;
+                        Printf.printf "sum = %f\t" !sum;
                         n.value <- (this#sigma !sum);
+                        Printf.printf "n%d -> %f\n" i n.value;
                         sum := 0.;
-                        n.version <- n.version + 1
-                    ) neurons
+                        n.version <- n.version + 1;
+                    ) neurons;
+                    print_newline ();
                 in 
-                    recalculate hidden; 
+                    Printf.printf "Hidden neurons :\n";
+                    recalculate hidden;
+                    Printf.printf "Output neurons :\n";
                     recalculate outputs
+
+        method get_outputs =
+            let l = Array.length outputs in
+                Array.init l (fun i -> outputs.(i).value)
 
         method outputsBackpropagation (n : neuron) (expected : float) = 
             let delta = (n.value *. (1. -. n.value) *. (expected -. n.value)) *. epsilon *. n.value
@@ -60,9 +73,9 @@ class network epsilon (inputs : neuron array) (outputs : neuron array) (hidden :
 
         method train (input : float array) (output : float array) = (
             this#set_values(input);
-            Array.iteri (fun i v ->
+            (*Array.iteri (fun i v ->
                 this#outputsBackpropagation v output.(i)
-            ) outputs
+            ) outputs*)
         )
     
     end
@@ -70,13 +83,15 @@ class network epsilon (inputs : neuron array) (outputs : neuron array) (hidden :
 let new_network () = (
     Random.self_init();
     let nn i = new_neuron () in
-    let inputs  = Array.init (28*28) nn in
-    let outputs = Array.init (2*26 + 10 + 1) nn in
-    let hidden  = Array.init 96 nn in
+    let inputs  = Array.init 2 nn in
+    let outputs = Array.init 1 nn in
+    let hidden  = Array.init 2 nn in
     let populate l1 l2 = (
         Array.iter (fun entry -> 
             Array.iter (fun exit -> 
-                entry.children <- {weight = Random.float 1.; entry = entry; exit = exit}::entry.children
+                let w = Random.float 1. in
+                    entry.children <- {weight = w; entry = entry; exit = exit}::entry.children;
+                    exit.parents <- {weight = w; entry = entry; exit = exit}::exit.parents;
             ) l2
         ) l1;
     ) in
@@ -85,4 +100,3 @@ let new_network () = (
 
         new network 0.25 inputs outputs hidden;
 )
-
