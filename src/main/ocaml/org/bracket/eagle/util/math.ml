d41a8066015f3d93679c1355f3ad99b60c9cdbb6
Snaipe <franklinmathieu@gmail.com>
2013-10-18 21:20:00 +0200

    Added math utilities

---
+++ src/main/ocaml/org/bracket/eagle/util/math.ml
@@ -0,0 +1,26 @@
+module Logic = struct
+    let int_of_bool a = if a then 1 else 0
+    let bool_of_int a = a = 1
+    let iob = int_of_bool
+    let boi = bool_of_int
+    let (&) a b = boi (iob a land iob b)
+    let (|+) a b = boi (iob a lor iob b) (* sigh... why did | had to have a
+    special meaning... oh well.*)
+    let (&=) a b = a := !a & b
+    let (|=) a b = a := !a |+ b
+end
+
+module Universe = struct
+    let ( +=  ) a b = a := !a + b; !a
+    let ( +.= ) a b = a := !a +. b; !a
+    let ( -=  ) a b = a := !a - b; !a
+    let ( -.= ) a b = a := !a -. b; !a
+    let ( *=  ) a b = a := !a * b; !a
+    let ( *.= ) a b = a := !a *. b; !a
+    let ( /=  ) a b = a := !a / b; !a
+    let ( /.= ) a b = a := !a /. b; !a
+    let ( ~++ ) a = a += 1
+    let (  ++ ) = (~++)
+    let ( ~-- ) a = a -= 1
+    let (  -- ) = (~--)
+end
