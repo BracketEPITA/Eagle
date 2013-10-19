1e2ec511940f86105a9080f0552ebf31a15bb72a
KingGreed <gauber_r@epita.fr>
2013-10-19 13:05:16 +0200

    Remove unused code & formatted functions to be in english

--- src/main/ocaml/org/bracket/eagle/image/binarisation.ml
+++ src/main/ocaml/org/bracket/eagle/image/binarisation.ml
@@ -17,7 +17,7 @@ let rec wait_key () =
  
 (*
   show img dst
-  affiche la surface img sur la surface de destination dst (normalement l'écran)
+  affiche la surface img sur la surface de destination dst (normalement l'ecran)
 *)
 let show img dst =
   let d = Sdlvideo.display_format img in
@@ -27,8 +27,6 @@ let show img dst =
 let level (r,g,b) = (0.3*.float_of_int r +. 0.59*.float_of_int g
 +.0.11*.float_of_int b)
 
-let color2grey (r,g,b) = let l = int_of_float (level (r,g,b)) in (l,l,l)
-
 let imageiter f img = let (w,h) = get_dims img in
 for j=0 to h-1 do
         for i=0 to w-1 do
@@ -36,11 +34,7 @@ for j=0 to h-1 do
         done
 done
 
-
-let image2grey img dst = imageiter (fun x y -> Sdlvideo.put_pixel_color dst x y (color2grey(Sdlvideo.get_pixel_color img x y))) img
-
-
-let seuil img = 
+let threshold img = 
 	let (w,h) = get_dims img in
 	let sum = ref 0. in 
 		for j = 0 to h-1 do
@@ -52,18 +46,18 @@ let seuil img =
 	
 
 let binarisation img dst =
-    let seuil = seuil img in 
+    let threshold = threshold img in 
     imageiter (
         fun x y -> Sdlvideo.put_pixel_color dst x y (
-            if(level(Sdlvideo.get_pixel_color img x y)) < seuil then (0,0,0) 
+            if(level(Sdlvideo.get_pixel_color img x y)) < threshold then (0,0,0) 
             else (255,255,255)
         )
     ) img
 
 
 
-let makelistofblack img = 
-    let l = ref [] and seuil = seuil img in 
+let makeListOfBlack img = 
+    let l = ref [] and threshold = threshold img in 
     imageiter (
-        fun x y -> if(level(Sdlvideo.get_pixel_color img x y) > seuil) then l:= (x,y)::!l
+        fun x y -> if(level(Sdlvideo.get_pixel_color img x y) > threshold) then l:= (x,y)::!l
      ) img  
