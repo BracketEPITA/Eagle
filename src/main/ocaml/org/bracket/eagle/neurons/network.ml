d90602c154ae33dcd071fd176c63ab90a180eee6
Snaipe <franklinmathieu@gmail.com>
2013-12-07 18:09:24 +0100

    Added serialization

--- src/main/ocaml/org/bracket/eagle/neurons/network.ml
+++ src/main/ocaml/org/bracket/eagle/neurons/network.ml
@@ -6,27 +6,30 @@ module ActivationFunction = struct
     type func = {
         f : (float -> float);
         derivative : (float -> float);
+        name : string;
     }
-    let linear = {
-        f = (fun (x : float) -> x);
-        derivative = (fun x -> 1.);
-    }
-    let sigmoid = {
-        f = (fun x -> 1. /. (1. +. exp(-. x)));
-        derivative = (fun x -> x *. (1. -. x) +. 0.1);
-    }
-    let tanh = {
-        f = (fun x -> tanh x);
-        derivative = (fun x -> 1. -. x *. x);
-    }
-    let sigmoid_alt = {
-        f = (fun x -> x /. sqrt (1. +. x *. x));
-        derivative = (fun x -> 
-            let xs = x *. x in
-            let xs2 = (xs +. 1.) *. (xs +. 1.) in 
-                (1. -. xs) /. xs2
-        )
-    }
+    let get = 
+        let functions = Hashtbl.create 10 in
+        Hashtbl.add functions "linear" {
+            f = (fun (x : float) -> x);
+            derivative = (fun x -> 1.);
+            name = "linear";
+        };
+        Hashtbl.add functions "sigmoid" {
+            f = (fun x -> 1. /. (1. +. exp(-. x)));
+            derivative = (fun x -> x *. (1. -. x) +. 0.1);
+            name = "sigmoid";
+        };
+        Hashtbl.add functions "tanh" {
+            f = (fun x -> tanh x);
+            derivative = (fun x -> 1. -. x *. x);
+            name = "tanh";
+        };
+        fun name -> Hashtbl.find functions name
+
+    let linear  = get "linear"
+    let sigmoid = get "sigmoid"
+    let tanh    = get "tanh"
 end
 
 type dataset = {
@@ -110,32 +113,3 @@ class basic_network (data : network_data) =
         )
 
     end
-
-let new_network () =
-    Random.self_init ();
-    let l = Array.length in
-    let layers = [|
-        Array.make 50 0.;
-        Array.make 10 0.;
-        Array.make 4 0.
-    |] in
-    for i = 0 to l layers - 2 do
-        layers.(i).(l layers.(i) - 1) <- 1.;
-    done;
-    let weights = Array.init (l layers - 1) (fun i -> 
-        let l1 = l layers.(i) and l2 = l layers.(i + 1) in
-        let s = l1 * (l2 + if i = l layers - 2 then 0 else -1) in
-        Array.init s (fun j ->
-            Random.float 1.
-        )
-    ) in
-    let activations = [|
-        ActivationFunction.sigmoid;
-        ActivationFunction.sigmoid
-    |] in
-    let data = {
-        layers = layers; 
-        weights = weights; 
-        activations = activations
-    } in
-        new basic_network data;
